10c7e904a59faf1e17cadc695e8281e9cfb3f399
chimekinglsey <onlinekingsley@gmail.com>
2023-05-14 23:22:07 +0000

    new commit on pycode

--- pycode.py
+++ pycode.py
@@ -3,22 +3,25 @@ import random
 """This code is written to pass pycode
 test and nothing serious"""
 
-secretNumber = random.randint(1, 20)
-print('I am thinking of a number between 1 and 20.')
 
-# Ask the player to guess 6 times.
-for guessesTaken in range(1, 7):
-    print('Take a guess.')
-    guess = int(input())
+def your_luck():
+    """this function helps to randomly check how lucky you are"""
+    secretNumber = random.randint(1, 20)
+    print('I am thinking of a number between 1 and 20.')
 
-    if guess < secretNumber:
-        print('Your guess is too low.')
-    elif guess > secretNumber:
-        print('Your guess is too high.')
+    # Ask the player to guess 6 times.
+    for guessesTaken in range(1, 7):
+        print('Take a guess.')
+        guess = int(input())
+
+        if guess < secretNumber:
+            print('Your guess is too low.')
+        elif guess > secretNumber:
+            print('Your guess is too high.')
+        else:
+            break    # This condition is the correct guess!
+
+    if guess == secretNumber:
+        print('Good job! You guessed my number')
     else:
-        break    # This condition is the correct guess!
-
-if guess == secretNumber:
-    print('Good job! You guessed my number')
-else:
-    print('Nope. The number I was thinking of was ' + str(secretNumber))
+        print('Nope. The number I was thinking of was ' + str(secretNumber))
