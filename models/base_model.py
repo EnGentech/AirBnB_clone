de5a53c6370bb284dd229eabae33682280ad1173
chimekinglsey <onlinekingsley@gmail.com>
2023-05-14 22:36:12 +0000

    PycodeStyle check

--- models/base_model.py
+++ models/base_model.py
@@ -4,32 +4,33 @@ from datetime import datetime
 import uuid
 from models import storage
 """This program defines the console/command line
-	interpreter for the AirBnB project
+    interpreter for the AirBnB project
 """
+
 
 class BaseModel:
     """
-	BaseModel class defines the  attributes and methods
-	to be inherited by childclasses and instances
+    BaseModel class defines the  attributes and methods
+    to be inherited by childclasses and instances
 
-	@args:
-		`id`: assigns a universally unique identity for every Basemodel object
-		`created_at`: this assigns the current time of creation for every
-					object or child class of BaseModel
-		`updated_at`: this initially takes same value as `created_at` and 
-					keeps track of the time of any update on attributes
-	"""
+    @args:
+        `id`: assigns a universally unique identity for every Basemodel object
+        `created_at`: this assigns the current time of creation for every
+        object or child class of BaseModel
+        `updated_at`: this initially takes same value as `created_at` and
+        keeps track of the time of any update on attributes
+    """
     global for_mat
     for_mat = "%Y-%m-%dT%H:%M:%S.%f"
 
     def __init__(self, *args, **kwargs):
         """
-		An instantiation of the Base Model
+        An instantiation of the Base Model
 
-		@args:
-			`args`: takes variable number of argument
-			`kwargs`: takes a key-value pair of argument in the form of a dict
-		"""
+        @args:
+            `args`: takes variable number of argument
+            `kwargs`: takes a key-value pair of argument in the form of a dict
+        """
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -44,35 +45,35 @@ class BaseModel:
 
     def __str__(self):
         """String representation of the BaseModel
-		`cls_name` is a variable that stores the name of our current class
-			It's content is the class name of the instance class or object.
-		`return`: it returns a printable format like this: 
-				[<class name>] (<self.id>) <self.__dict__>
-		"""
+        `cls_name` is a variable that stores the name of our current class
+        It's content is the class name of the instance class or object.
+        `return`: it returns a printable format like this:
+        [<class name>] (<self.id>) <self.__dict__>
+        """
         cl_name = self.__class__.__name__
         return "[{}] ({}) {}".format(cl_name, self.id, self.__dict__)
 
     def save(self):
         """
-		Updating time and date at the point of saving
-		When this method is called, current datetime is 
-			assigned to `updated_at'
-		`return`: returns the time that last call to save method occurred
-		"""
+        Updating time and date at the point of saving
+        When this method is called, current datetime is
+            assigned to `updated_at'
+        `return`: returns the time that last call to save method occurred
+        """
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
         """
-		Redefining dictionary
-		This method makes a copy of the self.__dict__ of the calling subclass
-		Then updates it's own dict without affecting the initial self.__dict__
-		@args:
-			`__class__`: key with value as class name
-			`created_at`: key with isoformat str rep of creation datetime
-			`updated_at`: key with isoformat str rep of updation datetime
-			`return`: updated dictionary with three added key-value pairs
-		"""
+        Redefining dictionary
+        This method makes a copy of the self.__dict__ of the calling subclass
+        Then updates it's own dict without affecting the initial self.__dict__
+        @args:
+            `__class__`: key with value as class name
+            `created_at`: key with isoformat str rep of creation datetime
+            `updated_at`: key with isoformat str rep of updation datetime
+            `return`: updated dictionary with three added key-value pairs
+        """
         my_dict = self.__dict__.copy()
         my_dict.update({
             "__class__": self.__class__.__name__,
