37ed8c2bd684bc8ecac2f6658fe1efbcf1e647c7
EnGentech <engen.inyang@gmail.com>
2023-05-15 16:48:37 -0700

    tried

--- console.py
+++ console.py
@@ -20,11 +20,11 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnd) "
 
     def do_quit(self, arg):
-        '''Quit command to exit the program\n'''
+        '''Quit command to exit the program'''
         return True
 
     def do_EOF(self, arg):
-        """Quit command to exit the program\n"""
+        """Quit command to exit the program"""
         return True
 
     def emptyline(self):
