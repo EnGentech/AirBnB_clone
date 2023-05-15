183623133f3938f113730088ffa7d7c54654a1ed
chimekinglsey <onlinekingsley@gmail.com>
2023-05-15 22:11:06 +0000

    User test file

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Testing users"""
+import unittest
+from models.user import User
+
+class TestUser(unittest.TestCase):
+	"""This is test for user class"""
+	user = User()
+	def test_user(self):
+		"""testing user class"""
+		self.assertTrue(User())
+	
+	def test_attr(self):
+		"""testing the attributes of test class"""
+		self.assertTrue(self.user.email=='')
+		self.assertTrue(self.user.password=='')
+		self.assertTrue(self.user.first_name=='')
+		self.assertTrue(self.user.last_name=='')
+		#self.assertTrue(hasattr(self.user, "email"))
+		#self.assertTrue(hasattr(self.user, "password"))
+		#self.assertTrue(hasattr(self.user, "first_name"))
+		#self.assertTrue(hasattr(self.user, "last_name"))
+
+	def test_attr_type(self):
+		"""testing the attributes of test class"""
+		self.assertTrue(isinstance(self.user.password, str))
+		self.assertTrue(isinstance(self.user.email, str))
+		self.assertTrue(isinstance(self.user.first_name, str))
+		self.assertTrue(isinstance(self.user.last_name, str))
