8db1367fb02f2c281eca20d378f010cb31756bc1
EnGentech <engen.inyang@gmail.com>
2023-05-15 16:38:35 -0700

    modified content

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
 """Testing users"""
 import unittest
-from models.user import User
+from models.engine.file_storage import FileStorage
+
 
 class TestUser(unittest.TestCase):
-	"""This is test for user class"""
-	user = User()
-	def test_user(self):
-		"""testing user class"""
-		self.assertTrue(User())
-	
-	def test_attr(self):
-		"""testing the attributes of test class"""
-		self.assertTrue(self.user.email=='')
-		self.assertTrue(self.user.password=='')
-		self.assertTrue(self.user.first_name=='')
-		self.assertTrue(self.user.last_name=='')
-		#self.assertTrue(hasattr(self.user, "email"))
-		#self.assertTrue(hasattr(self.user, "password"))
-		#self.assertTrue(hasattr(self.user, "first_name"))
-		#self.assertTrue(hasattr(self.user, "last_name"))
+    """This is test for user class"""
+    user = FileStorage()
 
-	def test_attr_type(self):
-		"""testing the attributes of test class"""
-		self.assertTrue(isinstance(self.user.password, str))
-		self.assertTrue(isinstance(self.user.email, str))
-		self.assertTrue(isinstance(self.user.first_name, str))
-		self.assertTrue(isinstance(self.user.last_name, str))
+    def test_File_storage(self):
+        """testing user class"""
+        self.assertTrue(FileStorage())
+
+    def test_objectType(self):
+        """checking if the attribute __object exist"""
+        self.assertTrue(hasattr(self.user, FileStorage.__objects))
+        # self.assertTrue(hasattr(self.user.__file_path))
+
+    def test_all(self):
+        """checking if all method exist"""
+        self.assertTrue(hasattr(self.user, "all"))
+
+    def test_new(self):
+        """checking if the method new exist"""
+        self.assertTrue(hasattr(self.user, "new"))
+
+    def test_save(self):
+        """check for save method in file_storage"""
+        self.assertTrue(hasattr(self.user, "save"))
+
+    def test_reload(self):
+        """check for the existence of reload in file_storage"""
+        self.assertTrue(hasattr(self.user, "reload"))
